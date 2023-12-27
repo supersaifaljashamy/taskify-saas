8539e5738937ea414ac2e516d5c836f076e82bdb
Saif Aljashamy <supersaifaljashamy@gmail.com>
2023-12-26 21:48:38 -0800

    07: website logo & name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,24 @@
+import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import { siteConfig } from '@/config/site'
+
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: [
+    {
+      url: "/logo.svg",
+      href: "/logo.svg"
+    }
+  ]
+}
 
 export default function RootLayout({
   children,
