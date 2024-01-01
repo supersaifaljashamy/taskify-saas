19c42af0dceba26172bbff3cf10536b2d907d56b
Saif Aljashamy <supersaifaljashamy@gmail.com>
2023-12-31 18:04:09 -0800

    17: field errors

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,15 @@
 const OrganizationIdPage = async () => {
     return (
     <div>
-        Organization Page
+        <form>
+            <input 
+                id="title"
+                name="title"
+                required
+                placeholder="Enter a board title"
+                className="border-black border p-1"
+            />
+        </form>
     </div>
     );
 };
