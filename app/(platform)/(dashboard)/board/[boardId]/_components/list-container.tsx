8270cbb83e1540a1eff86d8fb9cfa89d4a7a3cb8
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-01-08 15:47:20 -0800

    32: display lists

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import { ListWithCards } from "@/types";
 import { ListForm } from "./list-form";
+import { ListItem } from "./list-item";
 
 
 interface ListContainerProps {
@@ -14,8 +17,23 @@ export const ListContainer = ({
   data,
   boardId,
 }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data);
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
   return (
-    <ol>
+    <ol className="flex gap-x-3 h-full">
+        {orderedData.map((list, index) => {
+          return(
+            <ListItem 
+              key={list.id}
+              index={index}
+              data={list}
+            />
+          )
+        })}
         <ListForm />
         <div className="flex-shrink-0 w-1" />
     </ol>
