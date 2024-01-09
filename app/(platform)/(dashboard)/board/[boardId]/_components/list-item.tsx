4d9aaaeaddc5a12ceec6481d271bcae60fe28ca6
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-01-08 19:27:30 -0800

    33: header to display list title

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -2,6 +2,8 @@
 
 import { ListWithCards } from "@/types";
 
+import { ListHeader } from "./list-header";
+
 interface ListItemProps {
     data: ListWithCards;
     index: number;
@@ -13,8 +15,8 @@ export const ListItem = ({
 }: ListItemProps) => {
     return(
         <li className="shrink-0 h-full w-[272px] select-none">
-            <div className="w-full rounded-md bg-{#1f1f2f4] shadow-md pb-2">
-            
+            <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
+                <ListHeader data={data} />
             </div>
         </li>
     )
