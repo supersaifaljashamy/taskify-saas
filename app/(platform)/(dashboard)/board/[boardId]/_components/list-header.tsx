714f1dc3ce21b5583aa9049c4c92503d73cfe2f1
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-01-08 19:48:03 -0800

    34: enable editing list title on click

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { List } from "@prisma/client";
+import { useEventListener } from "usehooks-ts";
+import { useState, useRef, ElementRef } from "react";
 
 interface ListHeaderProps {
     data: List;
@@ -9,11 +11,43 @@ interface ListHeaderProps {
 export const ListHeader = ({
     data,
 } : ListHeaderProps) => {
-  return (
+    const [title, setTitle] = useState(data.title);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const formRef = useRef<ElementRef<"form">>(null);
+    const inputRef = useRef<ElementRef<"input">>(null);
+    
+    const enableEditing = () => {
+        setIsEditing(true);
+        setTimeout(() => {
+            inputRef.current?.focus();
+            inputRef.current?.select();
+        })
+    };
+
+    const disableEditing = () => {
+        setIsEditing(false);
+    };
+
+    const onKeyDown = (e: KeyboardEvent) => {
+        if(e.key == "Escape"){
+            formRef.current?.requestSubmit();
+        }
+    };
+
+    useEventListener("keydown", onKeyDown);
+    return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
-        <div className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent">
-            {data.title}
-        </div>
+        {isEditing ? (
+            <p>Form</p>
+        ) : (
+            <div 
+                onClick={enableEditing}
+                className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent"
+            >
+                {title}
+            </div>  
+        )}
     </div>
-  );
+    );
 };
