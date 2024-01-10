24df7ea84bd06542c132044737724b3c72b4009b
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-01-09 20:07:34 -0800

    35: display editing list title functionality

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -4,6 +4,8 @@ import { List } from "@prisma/client";
 import { useEventListener } from "usehooks-ts";
 import { useState, useRef, ElementRef } from "react";
 
+import { FormInput } from "@/components/form/form-input";
+
 interface ListHeaderProps {
     data: List;
 };
@@ -39,7 +41,19 @@ export const ListHeader = ({
     return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
         {isEditing ? (
-            <p>Form</p>
+            <form className="flex-1 px-[2px]">
+                <input hidden id="id" name="id" value={data.id} />
+                <input hidden id="boardId" name="boardId" value={data.boardId} />
+                <FormInput
+                    ref={inputRef}
+                    onBlur={() => {}}
+                    id="title"
+                    placeholder="Enter list title..."
+                    defaultValue={title}
+                    className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
+                />
+
+            </form>
         ) : (
             <div 
                 onClick={enableEditing}
