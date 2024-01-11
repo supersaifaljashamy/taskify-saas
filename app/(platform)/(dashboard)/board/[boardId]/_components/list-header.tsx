b7225232dc445b0ce387e80033f3eb69b58ed8fb
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-01-11 14:47:12 -0800

    40: list options component

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -8,6 +8,8 @@ import { useAction } from "@/hooks/use-action";
 import { updateList } from "@/actions/update-list";
 import { FormInput } from "@/components/form/form-input";
 
+import { ListOptions } from "./list-options";
+
 interface ListHeaderProps {
     data: List;
 };
@@ -98,6 +100,10 @@ export const ListHeader = ({
                 {title}
             </div>  
         )}
+        <ListOptions
+            onAddCard={() => {}}
+            data={data}
+        />
     </div>
     );
 };
