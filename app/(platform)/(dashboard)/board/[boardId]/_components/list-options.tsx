2e320984297b5c0e7980a8a64b0e7af0dbdfe2c5
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-01-12 15:30:21 -0800

    43: integrate action into delete list form

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import { ElementRef, useRef } from "react";
 import { List } from "@prisma/client";
 import { MoreHorizontal, X } from "lucide-react";
+
+import { useAction } from "@/hooks/use-action";
+import { deleteList } from "@/actions/delete-list";
 
 import {
     Popover,
@@ -22,51 +26,68 @@ export const ListOptions = ({
     data,
     onAddCard,
 }: ListOptionsProps) => {
-  return (
-    <Popover> 
-        <PopoverTrigger asChild>
-            <Button className="h-auto w-auto p-2" variant="ghost">
-                <MoreHorizontal className="h-4 w-4" />
-            </Button>
-        </PopoverTrigger>
-        <PopoverContent className="px-0 pt-3 pb-3" side="bottom" align="start">
-            <div className="text-sm font-medium text-center text-neutral-600 pb-4">
-                List actions
-            </div>
-            <PopoverClose asChild>
-                <Button className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600" variant="ghost">
-                    <X className="h-4 w-4"/>
+    const closeRef = useRef<ElementRef<"button">>(null);
+
+    const { execute: executeDelete } = useAction(deleteList, {
+        onSuccess: (data) => {
+            closeRef.current?.click();
+        },
+        onError: (error) => {},
+    });
+
+    const onDelete = (formData: FormData) => {
+        const id = formData.get("id") as string;
+        const boardId = formData.get("boardId") as string;
+
+        executeDelete({id, boardId});
+    }
+    return (
+        <Popover> 
+            <PopoverTrigger asChild>
+                <Button className="h-auto w-auto p-2" variant="ghost">
+                    <MoreHorizontal className="h-4 w-4" />
                 </Button>
-            </PopoverClose>
-            <Button
-                onClick={onAddCard}
-                className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
-                variant="ghost"
-            >
-                Add card...
-            </Button>
-            <form>
-                <input hidden name="id" id="id" value={data.id} />
-                <input hidden name="boardId" id="boardId" value={data.boardId} />
-                <FormSubmit
+            </PopoverTrigger>
+            <PopoverContent className="px-0 pt-3 pb-3" side="bottom" align="start">
+                <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+                    List actions
+                </div>
+                <PopoverClose ref={closeRef} asChild>
+                    <Button className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600" variant="ghost">
+                        <X className="h-4 w-4"/>
+                    </Button>
+                </PopoverClose>
+                <Button
+                    onClick={onAddCard}
                     className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
                     variant="ghost"
                 >
-                    Copy list...
-                </FormSubmit>
-            </form>
-            <Separator />
-            <form>
-                <input hidden name="id" id="id" value={data.id} />
-                <input hidden name="boardId" id="boardId" value={data.boardId} />
-                <FormSubmit
-                    className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
-                    variant="ghost"
+                    Add card...
+                </Button>
+                <form>
+                    <input hidden name="id" id="id" value={data.id} />
+                    <input hidden name="boardId" id="boardId" value={data.boardId} />
+                    <FormSubmit
+                        className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+                        variant="ghost"
+                    >
+                        Copy list...
+                    </FormSubmit>
+                </form>
+                <Separator />
+                <form
+                    action={onDelete}
                 >
-                    Delete this list
-                </FormSubmit>
-            </form>
-        </PopoverContent>
-    </Popover>
-  );
+                    <input hidden name="id" id="id" value={data.id} />
+                    <input hidden name="boardId" id="boardId" value={data.boardId} />
+                    <FormSubmit
+                        className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+                        variant="ghost"
+                    >
+                        Delete this list
+                    </FormSubmit>
+                </form>
+            </PopoverContent>
+        </Popover>
+    );
 };
