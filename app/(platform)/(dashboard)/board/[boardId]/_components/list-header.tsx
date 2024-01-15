4f33a1312e7b822e86083ad88936772689d54fc2
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-01-14 21:51:38 -0800

    46: display add card under list

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -12,10 +12,12 @@ import { ListOptions } from "./list-options";
 
 interface ListHeaderProps {
     data: List;
+    onAddCard: () => void;
 };
 
 export const ListHeader = ({
     data,
+    onAddCard,
 } : ListHeaderProps) => {
     const [title, setTitle] = useState(data.title);
     const [isEditing, setIsEditing] = useState(false);
@@ -101,7 +103,7 @@ export const ListHeader = ({
             </div>  
         )}
         <ListOptions
-            onAddCard={() => {}}
+            onAddCard={onAddCard}
             data={data}
         />
     </div>
