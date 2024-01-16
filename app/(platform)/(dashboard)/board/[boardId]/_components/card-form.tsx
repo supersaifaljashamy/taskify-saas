e55c3e13bc553447c3c774d76015d2b505847488
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-01-16 15:07:12 -0800

    51: integrate action into create card form

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -7,7 +7,11 @@ import {
   ElementRef, 
   KeyboardEventHandler,
 } from "react";
+import { useParams } from "next/navigation";
+import { useOnClickOutside, useEventListener } from "usehooks-ts";
 
+import { useAction } from "@/hooks/use-action";
+import { createCard } from "@/actions/create-card";
 import { Button } from "@/components/ui/button";
 import { FormTextarea } from "@/components/form/form-textarea";
 import { FormSubmit } from "@/components/form/form-submit";
@@ -25,14 +29,51 @@ export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(({
   disableEditing,
   isEditing,
 }, ref) => {
+  const params = useParams();
+  const formRef = useRef<ElementRef<"form">>(null);
+
+  const { execute, fieldErrors } = useAction(createCard, {
+    onSuccess: (data) => {
+      formRef.current?.reset();
+    },
+    onError: (error) => {
+    },
+  });
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      disableEditing();
+    }
+  };
+
+  useOnClickOutside(formRef, disableEditing);
+  useEventListener("keydown", onKeyDown);
+
+  const onTextareakeyDown: KeyboardEventHandler<HTMLTextAreaElement> = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      formRef.current?.requestSubmit();
+    }
+  };
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const listId = formData.get("listId") as string;
+    const boardId = params.boardId as string;
+
+    execute({ title, listId, boardId });
+  };
+
   if(isEditing) {
     return (
       <form
+        ref={formRef}
+        action={onSubmit}
         className="m-1 py-0.5 px-1 space-y-4"
       >
         <FormTextarea 
           id="title"
-          onKeyDown={() => {}}
+          onKeyDown={onTextareakeyDown}
           ref={ref}
           placeholder="Enter a title for this card..."
 
