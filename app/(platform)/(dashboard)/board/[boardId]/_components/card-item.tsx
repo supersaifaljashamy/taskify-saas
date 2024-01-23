2e7c77fa7bdca2837f8a49e9b3c82ac7c05a839f
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-01-22 22:07:27 -0800

    64: display card modal on click

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -3,6 +3,8 @@
 import { Card } from "@prisma/client";
 import { Draggable } from "@hello-pangea/dnd";
 
+import { useCardModal } from "@/hooks/use-card-modal";
+
 
 interface CardItemProps {
   data: Card;
@@ -13,6 +15,7 @@ export const CardItem = ({
   data,
   index,
 }: CardItemProps) => {
+  const cardModal = useCardModal();
 
   return (
     <Draggable draggableId={data.id} index={index}>
@@ -22,6 +25,7 @@ export const CardItem = ({
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           role="button"
+          onClick={() => cardModal.onOpen(data.id)}
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
         >
           {data.title}
