cf3e0e424709ab00f3b9e6639e0f5b56f674f9b9
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-01-24 18:35:18 -0800

    67: input to update card title

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { ElementRef, useRef, useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
 import { Layout } from "lucide-react";
 
 import { CardWithList } from "@/types";
@@ -14,19 +16,37 @@ interface HeaderProps {
 export const Header = ({
   data,
 } : HeaderProps) => {
+  const queryClient = useQueryClient();
+  const params = useParams();
+
+  const inputRef = useRef<ElementRef<"input">>(null);
+
   const[title, setTitle] = useState(data.title);
+
+  const onBlur = () => {
+    inputRef.current?.form?.requestSubmit();
+  }
+
+  const onSubmit = (formData: FormData) => {
+    console.log(formData.get("title"));
+  }
 
   return (
     <div className="flex items-start gap-x-3 mb-6 w-full">
       <Layout className="h-5 w-5 mt-1 text-neutral-700" />
       <div className="w-full">
-        <form>
+        <form action={onSubmit}>
           <FormInput
+            ref={inputRef}
+            onBlur={onBlur}
             id="title"
             defaultValue={title}
             className="ont-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
           />
         </form>
+        <p className="text-sm text-muted-foreground">
+            in list <span className="underline">{data.list.title}</span>
+        </p>  
       </div>
     </div>
   );
