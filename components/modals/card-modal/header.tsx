d9154f8e2c1a16310139ac17c0ca3e27641d0409
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-01-25 18:48:37 -0800

    69: integrate update card action

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { ElementRef, useRef, useState } from "react";
-import { useQueryClient } from "@tanstack/react-query";
-import { useParams } from "next/navigation";
 import { Layout } from "lucide-react";
+import { useParams } from "next/navigation";
+import { useQueryClient } from "@tanstack/react-query";
 
 import { CardWithList } from "@/types";
+import { useAction } from "@/hooks/use-action";
+import { updateCard } from "@/actions/update-card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { FormInput } from "@/components/form/form-input";
 
@@ -15,20 +17,43 @@ interface HeaderProps {
 
 export const Header = ({
   data,
-} : HeaderProps) => {
+}: HeaderProps) => {
   const queryClient = useQueryClient();
   const params = useParams();
 
+  const { execute } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["card", data.id]
+      });
+
+      setTitle(data.title);
+    },
+    onError: (error) => {
+    }
+  });
+
   const inputRef = useRef<ElementRef<"input">>(null);
 
-  const[title, setTitle] = useState(data.title);
+  const [title, setTitle] = useState(data.title);
 
   const onBlur = () => {
     inputRef.current?.form?.requestSubmit();
-  }
+  };
 
   const onSubmit = (formData: FormData) => {
-    console.log(formData.get("title"));
+    const title = formData.get("title") as string;
+    const boardId = params.boardId as string;
+
+    if (title === data.title) {
+      return;
+    }
+
+    execute({
+      title,
+      boardId,
+      id: data.id,
+    });
   }
 
   return (
@@ -41,12 +66,12 @@ export const Header = ({
             onBlur={onBlur}
             id="title"
             defaultValue={title}
-            className="ont-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
+            className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
           />
         </form>
         <p className="text-sm text-muted-foreground">
-            in list <span className="underline">{data.list.title}</span>
-        </p>  
+          in list <span className="underline">{data.list.title}</span>
+        </p>
       </div>
     </div>
   );
