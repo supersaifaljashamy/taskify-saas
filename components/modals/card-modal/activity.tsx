25216076fe7c400015061104b67621669f29629b
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-01-31 17:39:51 -0800

    83: generate log message

--- components/modals/card-modal/activity.tsx
+++ components/modals/card-modal/activity.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { AuditLog } from "@prisma/client";
+import { ActivityIcon } from "lucide-react";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import { ActivityItem } from "@/components/activity-item";
 
 interface ActivityProps {
   items: AuditLog[];
@@ -13,7 +15,20 @@ export const Activity = ({
 }: ActivityProps) => {
   return (
     <div className="flex items-start gap-x-3 w-full">
-      Activity
+      <ActivityIcon className="h-5 w-5 mt-0.5 text-neutral-700" />
+      <div className="w-full">
+        <p className="font-semibold text-neutral-700 mb-2">
+          Activity
+        </p>
+        <ol className="mt-2 space-y-4">
+          {items.map((item) => (
+            <ActivityItem
+              key={item.id}
+              data={item}
+            />
+          ))}
+        </ol>
+      </div>
     </div>
   );
 };
