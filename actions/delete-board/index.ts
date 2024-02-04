2cae5d0c91194946d848321ecffa367cfa7b16e4
Saif Aljashamy <supersaifaljashamy@gmail.com>
2024-02-03 23:23:41 -0800

    89: audit log for delete board

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -2,13 +2,15 @@
 
 import { auth } from "@clerk/nextjs";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import { createSafeAction } from "@/lib/create-safe-action";
 
 import { DeleteBoard } from "./schema";
 import { InputType, ReturnType } from "./types";
-import { redirect } from "next/navigation";
+import { createAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -29,6 +31,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         orgId,
       },
     });
+
+    await createAuditLog({
+      entityTitle: board.title,
+      entityId: board.id,
+      entityType: ENTITY_TYPE.BOARD,
+      action: ACTION.DELETE,
+    })
   } catch (error) {
     return {
       error: "Failed to delete."
